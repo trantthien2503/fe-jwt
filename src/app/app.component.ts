d0184d0381b25cd100d0fd2af10e6b3ff8f47cdd
trantthien2503 <49374309+trantthien2503@users.noreply.github.com>
2023-10-16 14:32:13 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './service/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'clientJWT';
+  username: string='admin';
+  password: string='password';
+  actors: any[];
+  films: any[];
+
+  constructor(public authService: AuthService) {
+    this.actors= [];
+    this.films = []
+  }
+
+  ngOnInit() {
+    this.authService.login(this.username, this.password).subscribe((response: any)=>{
+      if(response){
+        const { access_token } = response;
+        this.authService.saveToken(access_token);
+        this.loadActors();
+        this.loadFilms();
+      }
+    });
+
+  }
+
+  loadActors() {
+    this.authService.getActors().subscribe(
+      (response: any) => {
+        this.actors = response;
+      },
+      (error: any) => {
+        console.error(error);
+      }
+    );
+  }
+
+  loadFilms() {
+    this.authService.getFilms().subscribe(
+      (response) => {
+        this.films = response;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
 }
---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import * as jwt from 'jsonwebtoken';
+
+
+// Rest of your code...
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private SERVERB_URL = 'http://127.0.0.1:5000'; // Thay thế bằng URL thực tế của máy chủ
+
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string) {
+    return this.http.post<any>(`${this.SERVERB_URL}/login`, { username: username, password: password });
+  }
+
+  saveToken(token: string) {
+    localStorage.setItem('access_token', token);
+  }
+
+  // getDecodedToken() {
+  //   const token = localStorage.getItem('access_token');
+  //   if (token) {
+  //     return jwt_decode(token);
+  //   }
+  //   return null;
+  // }
+
+  isAuthenticated() {
+    const token = localStorage.getItem('access_token');
+    return token !== null;
+  }
+
+  logout() {
+    localStorage.removeItem('access_token');
+  }
+
+  getActors() {
+    return this.http.get<any[]>(`${this.SERVERB_URL}/actors`);
+  }
+
+  getFilms() {
+    return this.http.get<any[]>(`${this.SERVERB_URL}/films`);
+  }
+}
